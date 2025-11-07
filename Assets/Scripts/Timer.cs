4a7664c2279d03f456a3702547a3b857a48642e5
jjk23 <dlwogh2009@naver.com>
2025-11-07 14:19:53 +0900

    폰트추가+타이머 구현

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public TextMeshProUGUI text_Timer;
+    public float limitTime;//초 단위로 입력받음
+    public bool gameStart;
+
+    private void Update()
+    {
+        if(gameStart)
+        {
+            limitTime -= Time.deltaTime;
+            text_Timer.text = "0"+Mathf.Floor(limitTime/60) +" : "+ Mathf.Round(limitTime)%60;
+        }
+    }
+}
