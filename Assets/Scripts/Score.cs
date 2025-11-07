997d2ce647dae6bb582cff5e43c4c2b3890d0423
jjk23 <dlwogh2009@naver.com>
2025-11-07 17:59:45 +0900

    dotween에셋 다운,재료 옆에서 나오기 구현

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,23 +1,44 @@
 ﻿using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] List<Nutrient> FoodDB;
-    [SerializeField] float carbohydrate;
-    [SerializeField] float protein;
-    [SerializeField] float fat;
-    [SerializeField] float score;
-    [SerializeField] float finalScore;
+    [SerializeField] float carbohydrate=0;
+    [SerializeField] float protein=0;
+    [SerializeField] float fat=0;
+    [SerializeField] float score=0;
+    [SerializeField] float finalScore=0;
+    [SerializeField] TextMeshProUGUI finalScoreText;
+    public bool preference = false;
     public void AddFood(int index, float amount)//음식 인덱스와 양을 넣으면 영양소 값 더해짐
     {
         carbohydrate += FoodDB[index].carbohydrate * amount;
         protein+= FoodDB[index].protein * amount;
         fat+= FoodDB[index].fat * amount;
     }
-    private void Scoring()
+    public void Scoring()
     {
-
+        float total = carbohydrate + protein + fat;
+        carbohydrate = carbohydrate / total * 100;//백분위로 %표시
+        protein= protein / total * 100;
+        fat= fat / total * 100;
+        score = 1000 //영양소가 33%에서 벗어날수록 1%당 -50점
+            - Mathf.Abs(33 - carbohydrate)*50
+            - Mathf.Abs(33 - protein)*50
+            - Mathf.Abs(33 - fat)*50;
+        if(preference)//선호하면 +500점
+        {
+            score += 500;
+        }
+        preference = false;
+        carbohydrate = 0;
+        protein = 0;
+        fat = 0;
+        finalScore += score;
+        finalScoreText.text = ""+finalScore;
     }
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject Meats;
+    public GameObject Grains;
+    public GameObject Fruits;
+    public GameObject Dairy;
+
+    public void MovePlusX(GameObject obj)
+    {
+         obj.transform.DOLocalMoveX(-220, 2.0f);
+    }
+    public void MovePlusX2(GameObject obj)
+    {        
+         obj.transform.DOLocalMoveX(1620, 2.0f);
+    }
+    public void MoveMinusX(GameObject obj)
+    {      
+        obj.transform.DOLocalMoveX(-1610, 2.0f);
+    }
+    public void MoveMinusX2(GameObject obj)
+    {
+        obj.transform.DOLocalMoveX(220, 2.0f);
+    }
+}
