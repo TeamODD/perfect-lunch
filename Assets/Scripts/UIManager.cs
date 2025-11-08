baa25a0d7806d10355845d3cea4d4009e6a53827
wjsekdus <wjsekdus39@gmail.com>
2025-11-09 00:36:06 +0900

    도마위 재료 썰기

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -23,8 +23,8 @@ public class UIManager : MonoBehaviour
     [SerializeField] List<GameObject> customers;
     [SerializeField] List<GameObject> foodPrefabs;
     [SerializeField] List<int> randomList;//손님 인덱스를 랜덤으로 저장한 리스트
-    [SerializeField] int cnt=0;//손님 수
-    [SerializeField] TextMeshProUGUI CountText;
+    public int cnt=0;//손님 수
+    public TextMeshProUGUI CountText;
     [SerializeField] TextMeshProUGUI DialogueText;
     public bool iscook = false;//지금 재료손질 중인지
     public int cindex = 0;//cnt랑 다르게 0~3까지 하고 다시 0부터 시작
@@ -58,7 +58,16 @@ public class UIManager : MonoBehaviour
             score.index = index;
             Table.gameObject.SetActive(true);
             iscook = true;//현재 재료손질 중이라는 뜻
-            CookFood=Instantiate(foodPrefabs[index],Cook.transform);
+            //CookFood =Instantiate(foodPrefabs[index],Cook.transform);
+            CookFood = Instantiate(foodPrefabs[index]);
+            CookFood.transform.position = Cook.transform.position;
+            CookFood.name = "CookFood";
+            SpriteRenderer sr = CookFood.GetComponent<SpriteRenderer>();
+            if (sr != null)
+            {
+                sr.sortingLayerName = "Foreground"; // UI보다 앞에 나오도록
+                sr.sortingOrder = 10;
+            }
         }    
     }
     public void Shuffle()//마지막 인덱스가 처음으로 나오지 않게 손님순서 셔플
