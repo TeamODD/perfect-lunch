a64d3aad8acfb37064dff5482aab8eecb84441d5
jjk23 <dlwogh2009@naver.com>
2025-12-01 23:39:35 +0900

    크레딧추가+점수2초로

---
+++ Assets/Scripts/Credit.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class Credit : MonoBehaviour
+{
+    public GameObject credit;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClickCredit()
+    {
+        credit.SetActive(true);
+        credit.transform.localScale=new Vector3 (0f, 0f, 0f);
+        credit.transform.DOScale(new Vector3(1, 1, 1), 1f).SetEase(Ease.OutBack);
+    }
+    public void ExitCredit()
+    {
+        credit.SetActive (false);
+    }
+}
